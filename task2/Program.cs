cf86fc4eca44f112003bcaf42eac1682a79f78af
Tati <tanushka-p.94@mail.ru>
2022-09-06 00:09:11 +0300

    save2 task2

---
+++ task2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+void Fill(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            mtrx[i, j] = i+j;
+        }
+    }
+}
+
+void Print(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            Console.Write($"{mtrx[i, j]} ");
+        }
+        Console.Write("]");
+    Console.WriteLine(" ");
+    }
+}
+
+
+Console.WriteLine("m");
+int m = int.Parse(Console.ReadLine()); 
+Console.WriteLine("n");
+int n = int.Parse(Console.ReadLine());
+int[,] mtrx = new int [m,n];
+
+
+Fill(mtrx);
+Print(mtrx);
