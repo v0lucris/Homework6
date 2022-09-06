b5cf87fdd5fabe660acda856fe3e028760e2bd6a
Tati <tanushka-p.94@mail.ru>
2022-09-06 23:27:22 +0300

    save 1 task4

---
+++ task4/Program.cs
@@ -0,0 +1,62 @@
+﻿// Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+void Fill(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            mtrx[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void Print(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            Console.Write($"{mtrx[i, j]} ");
+        }
+        Console.Write("]");
+    Console.WriteLine(" ");
+    }
+}
+
+string Metod4 (int[,]mtrx)
+{
+    
+    string result = String.Empty;
+    for (int j = 0; j < mtrx.GetLength(1); j++)
+    {
+        int sum = 0;
+        int count = 0;
+        double res = 0;
+        for (int i = 0; i < mtrx.GetLength(0); i++)
+        {
+            sum = (sum + mtrx[i,j]);
+            count ++;
+        }
+       res = sum/count; 
+       result = result + res.ToString() + " ";
+    }
+    return result;
+}
+
+Console.WriteLine("m");
+int m = int.Parse(Console.ReadLine()); 
+Console.WriteLine("n");
+int n = int.Parse(Console.ReadLine());
+int[,] mtrx = new int [m,n];
+int sum = 0;
+Fill(mtrx);
+Console.WriteLine(" ");
+Print(mtrx);
+Console.WriteLine(" ");
+Console.WriteLine(Metod4(mtrx));
+
+
+
+
+
